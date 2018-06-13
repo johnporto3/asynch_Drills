9b1bee10465294c2ba69f139e9664f6aa21d958d
John Porto <john.porto@rocketmail.com>
2018-06-13 13:54:43 -0700

    working through callbacks

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-//Asynch: Now and later
+// //Asynch: Now and later
 
-//part 1&2
-let sun = (5);
-function message() {
-    console.log("I have", sun, "pairs of sunglasses.");
-}
-message();
+// //part 1&2
+// let sun = (5);
+// function message() {
+//     console.log("I have", sun, "pairs of sunglasses.");
+// }
+// message();
 
-//Part 3
-function multiply() {
-    console.log(sun * 2); 
-}
-multiply();
-// Part4
-setTimeout(() => {
-    console.log(sun * 2); 
-}, 2000);
+// //Part 3
+// function multiply() {
+//     console.log(sun * 2); 
+// }
+// multiply();
+// // Part4
+// setTimeout(() => {
+//     console.log(sun * 2); 
+// }, 2000);
 
 //Callbacks
 
+//1
 
+// function getWords() {
+//     console.log('funny')
+// }
+
+// setTimeout(() => {
+//     console.log('witty');
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log('hilarious');
+// }, 0);
+
+//     console.log('humorous');
+
+// getWords();
+
+// 2 & 3 I guessed funny, hilarious, humorous and then witty
+//      but it was humorous, funny, hilarious and then witty.
+
+//4
 
 
